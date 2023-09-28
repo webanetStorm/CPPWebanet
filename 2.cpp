e3d7c81cbc3b4ce5b9cf4a515bcc8c520f42e08f
webanet <my2712@mail.ru>
2023-09-28 18:12:18 +0300

    комит

--- 2.cpp
+++ 2.cpp
@@ -77,10 +77,8 @@ namespace WebanetB
 		cin >> n;
 
 		if ( n >= 1 )
-		{
 			for ( int i = n; i < n + 10; i++ )
 				cout << i << endl;
-		}
 		else
 			cout << "N должно быть натуральным числом\n";
 	}
--- 3.cpp
+++ 3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <math.h>
 #include <fstream>
 #include <string>
+#include <math.h>
 
 using namespace std;
 
