7c49d5a313dfb17997c3ddc3f04671252b7685f9
webanet <my2712@mail.ru>
2023-10-21 21:08:54 +0300

    кодировка

--- 2.cpp
+++ 2.cpp
@@ -7,7 +7,7 @@ using namespace std;
 namespace WebanetB
 {
 
-	float Pi() // ърёЄюьэр  ЇєэъЎш , тючтЁр∙р■∙р  ўшёыю яш
+	float Pi() // кастомная функция, возвращающая число пи
 	{
 		return 3.1415926535;
 	}
@@ -16,81 +16,81 @@ namespace WebanetB
 	{
 		float r1, r2, h;
 
-		cout << "╨рфшєё юёэютрэш  ъюэєёр: ";
+		cout << "Радиус основания конуса: ";
 		cin >> r1;
 
-		cout << "╨рфшєё ъЁ√°ш ъюэєёр: ";
+		cout << "Радиус крыши конуса: ";
 		cin >> r2;
 
-		cout << "┬т√ёюЄр ъюэєёр: ";
+		cout << "Ввысота конуса: ";
 		cin >> h;
 
 
 		float l = sqrt( pow( h, 2 ) + pow( r1 - r2, 2 ) );
 
-		cout << "╬с·╕ь: " << 1.0 / 3.0 * Pi() * h * ( pow( r1, 2 ) + r1 * r2 + pow( r2, 2 ) ); // pow() - Їєэъ. тючтхфхэш  т ёЄхяхэ№
-		cout << "\n╧ыю∙рф№ яютхЁїэюёЄш: " << Pi() * ( pow( r1, 2 ) + ( r1 + r2 ) * l + pow( r2, 2 ) ) << endl;
+		cout << "Объём: " << 1.0 / 3.0 * Pi() * h * ( pow( r1, 2 ) + r1 * r2 + pow( r2, 2 ) ); // pow() - функ. возведения в степень
+		cout << "\nПлощадь поверхности: " << Pi() * ( pow( r1, 2 ) + ( r1 + r2 ) * l + pow( r2, 2 ) ) << endl;
 	}
 
 	void FuncB()
 	{
 		float a, x;
 
-		cout << "╫шёыю A: ";
+		cout << "Число A: ";
 		cin >> a;
 
-		cout << "╫шёыю X: ";
+		cout << "Число X: ";
 		cin >> x;
 
 		cout << endl;
 
 
-		abs( x ) < 1 // abs() - ьюфєы№ ўшёыр
-			? x != 0 ? cout << "w = " << a * log( x ) << endl : cout << "═хъюЁЁхъЄэюх чэрўхэшх фы  X\n" // log() - эрЄєЁры№э√щ ыюурЁшЇь
-			: a - pow( x, 2 ) >= 0 ? cout << "w = " << sqrt( a - pow( x, 2 ) ) << endl : cout << "═хъюЁЁхъЄэ√х чэрўхэш  яхЁхьхээ√ї\n";
+		abs( x ) < 1 // abs() - модуль числа
+			? x != 0 ? cout << "w = " << a * log( x ) << endl : cout << "Некорректное значение для X\n" // log() - натуральный логарифм
+			: a - pow( x, 2 ) >= 0 ? cout << "w = " << sqrt( a - pow( x, 2 ) ) << endl : cout << "Некорректные значения переменных\n";
 	}
 
 	void FuncC()
 	{
 		float x, y, b;
 
-		cout << "╫шёыю X: ";
+		cout << "Число X: ";
 		cin >> x;
 
-		cout << "╫шёыю Y: ";
+		cout << "Число Y: ";
 		cin >> y;
 
-		cout << "╫шёыю B: ";
+		cout << "Число B: ";
 		cin >> b;
 
 		cout << endl;
 
 
-	    b >= x and b - y > 0 ? cout << log( b - y ) * sqrt( b - x ) << endl : cout << "═хъюЁЁхъЄэ√х чэрўхэш \n";
+	    b >= x and b - y > 0 ? cout << log( b - y ) * sqrt( b - x ) << endl : cout << "Некорректные значения\n";
 	}
 
 	void FuncD()
 	{
-		int n; // яхЁхьхээр  Єшяр int 
+		int n; // переменная типа int 
 
-		cout << "╫шёыю N: ";
+		cout << "Число N: ";
 		cin >> n;
 
 		if ( n >= 1 )
-			for ( int i = n; i < n + 10; i++ ) // Ўшъы ё ъюэъЁхЄэ√ь ъюы-тюь шЄхЁрЎшщ
+			for ( int i = n; i < n + 10; i++ ) // цикл с конкретным кол-вом итераций
 				cout << i << endl;
 		else
-			cout << "N фюыцэю с√Є№ эрЄєЁры№э√ь ўшёыюь\n";
+			cout << "N должно быть натуральным числом\n";
 	}
 
 	void FuncE()
 	{
 		for ( float x = -4; x <= 4; x += 0.5 )
 		{
-			if ( x == 1 ) // яЁюяєёъ x=1 тю шчсхцрэш  фхыхэш  эр 0
+			if ( x == 1 ) // пропуск x=1 во избежания деления на 0
 			{
-				cout << "╬°шсър\n";
-				continue; // ╧хЁхїюф эр ёыхфє■∙є■ шЄхЁрЎш■ Ўшъыр
+				cout << "Ошибка\n";
+				continue; // Переход на следующую итерацию цикла
 			}
 
 			cout << ( pow( x, 2 ) - 2 * x + 2 ) / ( x - 1 ) << endl;
